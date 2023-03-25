8be9b6903b6cf7933fde730a9b437a5e311cb298
Nawaf <nwf.aloufi@gmail.com>
2023-03-25 01:39:57 -0400

    fix mem_size fn

--- stage_2/src/mem.zig
+++ stage_2/src/mem.zig
@@ -1,38 +1,20 @@
 const write = @import("print.zig").write;
 pub export fn mem_size() u32 {
+    var ebx: u32 = 0;
+    var ecx: u32 = 0;
     asm volatile (
         \\ clc
         \\ xor %%ecx, %%ecx
-        \\ xor  %%edx, %%edx
+        \\ xor  %%ebx, %%ebx
         \\ mov  $0xE801, %%ax
         \\ int $0x15
         \\ jc err
+        : [r1] "={ebx}" (ebx),
+          [r2] "={ecx}" (ecx),
     );
-
-    return read_mem_size();
-}
-
-// ebx = memory above 16 mb in 64kb chunks
-// ecx = memory between 1mb and 16mb in kb
-export fn read_mem_size() u32 {
-    var ecx = read_ecx();
-    var ebx = read_ebx();
 
     var totalMemMb = ((ebx * 64) + ecx) / 1024;
-
     return totalMemMb;
-}
-
-fn read_ebx() u32 {
-    return asm volatile (""
-        : [ret] "={ebx}" (-> u32),
-    );
-}
-
-fn read_ecx() u32 {
-    return asm volatile (""
-        : [ret] "={ecx}" (-> u32),
-    );
 }
 
 export fn err() callconv(.Naked) void {
