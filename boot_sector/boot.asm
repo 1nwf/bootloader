824b6bc25ed7e380893b4ee0a3ece7403d836de6
Nawaf <nwf.aloufi@gmail.com>
2023-04-02 03:34:21 -0400

    set stack pointers and increase number of sectors loaded from disk for stage 2 to fix crash

--- boot_sector/boot.asm
+++ boot_sector/boot.asm
@@ -1,5 +1,7 @@
 [org 0x7c00]  ; where the boot sector will be loaded in memory
 STAGE2 equ 0x1000
+mov bp, 0x9000 ; set the stack 
+mov sp, bp
 mov bx, STR
 call print_str
 call load_second_stage
@@ -17,16 +19,16 @@ load_second_stage:
         pusha
 	mov bx, STAGE2
 
-	mov al, 15
-	mov dh, 0x00
-	mov ch, 0x00
-	mov cl, 0x02 ; sector after bootsector
+	mov al, 30 ; number of sectors to read
+	mov dh, 0x00 ; head number
+	mov ch, 0x00 ; cylinder number
+	mov cl, 0x02 ; sector number. 2 is the sector after the bootsector
 
         mov ah, 0x02 ;  bios read sectors function
         int 0x13  ; disk access interrupt
 
 	jc load_err
-	cmp al, 15
+	cmp al, 30 ; check that the number of sectors read matches our request
 	jne load_err
 
         popa
